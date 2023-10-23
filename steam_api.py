5f49539aea3881cd3c5f3df976864ead1e5dcee9
Kian <24872829@sun.ac.za>
2023-10-23 14:04:41 +0200

    Adedd flask for get_user_profile, also add requirements.txt

---
+++ steam_api.py
@@ -0,0 +1,21 @@
+import steam_scraper
+from flask import Flask, request, jsonify
+import os
+
+GITHUB_API_PORT = os.environ.get("STEAM_API_PORT", 5000)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Active'
+
+@app.route('/user/<name>', methods=['GET'])
+def get_user(name):
+    temp = (steam_scraper.get_user_profile(name))
+    if temp == None:
+        return None, 404
+    return temp, 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port = GITHUB_API_PORT)
